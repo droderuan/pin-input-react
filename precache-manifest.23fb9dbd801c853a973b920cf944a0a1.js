1e85ec20fab90f8209a04e7bd4fef16f613ce455
Ruan Ferreira <droderuan@hotmail.com>
2021-03-23 22:52:22 -0300

    Updates

---
+++ precache-manifest.23fb9dbd801c853a973b920cf944a0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a224915bf1445e20da66fc76c32549dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97804eefdfb7cf259a3f",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "d02e745bad336c96eb34",
+    "url": "./static/js/2.19137e26.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.19137e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97804eefdfb7cf259a3f",
+    "url": "./static/js/main.2f09e9eb.chunk.js"
+  },
+  {
+    "revision": "b6d6a3e4d2ca6fbe1396",
+    "url": "./static/js/runtime-main.275324ca.js"
+  }
+]);
