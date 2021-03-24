8bcb4a6a560ae66095de59e3c84ffd9da1dee87b
Ruan Ferreira <droderuan@hotmail.com>
2021-03-23 23:04:19 -0300

    Updated example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -14,8 +14,58 @@ const pin = () => (
   />
 );
 
+const pinOnFocus = () => (
+  <div style={{
+    textAlign: 'center',
+    width: 60,
+    height: 60,
+    marginRight: 15,
+    background: 'blue',
+  }}
+  />
+);
+
+const pinOnFilled: React.FC = ({ children }) => (
+  <div style={{
+    textAlign: 'center',
+    width: 60,
+    height: 60,
+    marginRight: 15,
+    background: 'green',
+  }}
+  >
+    {children}
+
+  </div>
+);
+
+const PinText: React.FC = ({ children }) => (
+  <p style={{
+    color: 'black',
+    fontSize: '19px',
+    fontWeight: 800,
+  }}
+  >
+    {children}
+
+  </p>
+
+);
+
+
 const App = () => {
-  return <PinInput direction="row" length={4}  pinComponent={pin} onChangePin={value => console.log(value)} />
+  return (
+  <PinInput
+    length={4}
+    onChangePin={(value) => console.log(value)}
+    showValues
+    pinText={PinText}
+    pinComponent={pin}
+    pinComponentOnFill={pinOnFilled}
+    pinComponentOnFocus={pinOnFocus}
+    direction="row"
+  />
+  )
 }
 
 export default App
